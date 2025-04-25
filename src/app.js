7c37203519cfdac0d1a93b679122082a310eaf63
Harshit Kharayat <harshitkharayat224@gmail.com>
2025-04-25 19:47:31 +0530

    exploring get post methods

--- src/app.js
+++ src/app.js
@@ -2,15 +2,21 @@ const express =require("express");
 
 const app=express();
 
-app.use("/hello",(req,res)=>{
-    res.send("Namaste Gentelman !")
+// app.get will only handle get call to  /user
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Harshit",lastName:"Kharayat",city:"Pithoragarh"})
+})
+app.post("/user",(req,res)=>{
+    res.send("successfully add the data to the database");
 })
 
+// app.use() will match all the http api method calls 
 app.use("/test",(req,res)=>{
     res.send("Hello Aliens how are you doing");
 })
+
 app.listen(3000,()=>{
     console.log("Successfully connected");
 });
 
- 
