49cc292a7d9e8398fbbf151d5f6de713f0bcfaa4
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-04-26 10:34:48 +0530

    error handling using app.use using try catch block

--- src/app.js
+++ src/app.js
@@ -5,24 +5,35 @@ const app=express();
 app.use("/admin",authAdmin)
 
 // app.get will only handle get call to  /user
-app.get("/admin/getData",(req,res,next)=>{
-    res.send("Successfully collected the user details");
+// app.get("/admin/getData",(req,res,next)=>{
+//     res.send("Successfully collected the user details");
    
-})  
+// })  
  
-app.get("/admin/deleteUser",(req,res)=>{
-    res.send("Deleted the user succesfully ");
+
+// app.get("/user",authUser,(req,res)=>{
+//     res.send("Hi Harshit!");
+// })
+
+
+// handling Errors
+
+
+app.get("/getUserData",(req,res,next)=>{
+
+    try{
+    throw new Error("----------error------");
+    res.send("user data found");
+    }catch(err){
+        res.status(500).send("something went worng")
+    }
+    
 })
 
-app.get("/user",authUser,(req,res)=>{
-    res.send("Hi Harshit!");
-})
-app.post("/user/login",(req,res)=>{
-    res.send("Logged In Succesfully!!");
-})
 
 
 app.listen(3000,()=>{
     console.log("Successfully connected");
+    
 });
 
