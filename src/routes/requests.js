0e8d9a94abec5bd98db1106a6e5bbe3902a79f7c
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-04-29 12:01:06 +0530

    create request review api

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -58,5 +58,40 @@ reqRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
 
 })
 
+reqRouter.post("/request/review/:status/:requestId",userAuth,async(req,res)=>{
+    try{    
+        const loggedInUser=req.user;
+        const {status,requestId}=req.params;
+        const allowedStatus=["accepted","rejected"];
+
+
+        if(!allowedStatus.includes(status)){
+            return res.status(400).send("Invalid Status Type :"+status);
+        }
+        const connectionRequest=await ConnectionRequest.findOne({
+            _id:requestId,
+            toUserId:loggedInUser._id,
+            status:"interested",
+        })
+        if(!connectionRequest){
+            return res.status(404).json({message:"Connection Request not found"});
+        }
+        connectionRequest.status=status;
+        const data=await connectionRequest.save();
+        res.json({
+            message:"Connection Request "+status,
+            data,
+        })
+         
+
+
+
+    }catch(err){
+        res.status(400).send("Something  went wrong !"+err.message)
+    }  
+    
+
+
+})
 
 module.exports=reqRouter
